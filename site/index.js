54f543293348f2e5f76e0c2a517c4315d97bf076
Matias <matias.mangano.1985@gmail.com>
2020-10-10 16:23:32 -0300

    primer commit

---
+++ site/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, ()=> {
+    console.log('Servidor Funcionando')
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html')
+})
+
+app.get('/productDetail', (req, res) => {
+    res.sendFile(__dirname + '/views/productDetail.html')
+})
+
+app.get('/productCart', (req, res) => {
+    res.sendFile(__dirname + '/views/productCart.html')
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html')
+})
