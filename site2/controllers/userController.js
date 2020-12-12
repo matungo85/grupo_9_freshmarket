69896b936658eac0d051353fedfa5001008627a5
Matias <matias.mangano.1985@gmail.com>
2020-12-12 01:33:39 -0300

    userController

--- site2/controllers/userController.js
+++ site2/controllers/userController.js
@@ -1,12 +1,78 @@
-var controller = {};
+const fs = require('fs');
+const path = require('path');
+const bcryptjs = require('bcryptjs');
+const { Console } = require('console');
 
+
+
+
+function getAllUsers () {
+    const usersFilePath = path.join(__dirname, '..', 'data/users.json');
+
+    const users = fs.readFileSync(usersFilePath, 'utf-8');
+    console.log(users)
+    if (users == '') {
+        return []
+    } else {
+        return JSON.parse(users)
+    }
+}
+
+function getNewId () {
+    const users = getAllUsers();
+    
+    if (users.length == 0) {
+        return 1
+    } else {
+        return users[users.length - 1].id + 1
+    }
+}
+
+function saveUsers (users) {
+
+    const usersFilePath = path.join(__dirname, '..', 'data/users.json');
+
+    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '))
+
+}
+
+
+var controller = {};
 controller = {
     login: function(req, res) {
         res.render('user/login');
     },
+
+    processLogin: function(req, res, next) {
+        
+    },
+
     register: function(req, res) {
         res.render('user/register');
     },
+
+    processRegister: function(req, res, next) {
+        
+    
+        const newUser = {
+            id: getNewId(),
+            name: req.body.name,
+            surname: req.body.surname,
+            email: req.body.email,
+            password: req.body.password,
+            category: 'user',
+            image: req.files[0].filename,
+            tel: req.body.tel,
+            dni: req.body.dni,
+            sex: req.body.sex,
+        }
+
+        const newUserList = getAllUsers().push(newUser);
+        
+        saveUsers(newUserList);
+
+    },
+
 }
 
 module.exports = controller;
--- site2/routes/users.js
+++ site2/routes/users.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var userController = require('../controllers/userController')
+var userController = require('../controllers/userController');
+const path = require('path')
+const multer = require('multer')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + '/../public/images/users')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+})
+ 
+var upload = multer({ storage: storage })
+
+
+router.get('/register', userController.register);
+
+router.post('/register',upload.any() ,userController.processRegister);
 
 router.get('/login', userController.login);
-router.get('/register', userController.register);
 
 module.exports = router;
