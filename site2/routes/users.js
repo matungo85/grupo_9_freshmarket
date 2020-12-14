2cc760969ce82a8714e275b536d6349d1b4325ad
Matias <matias.mangano.1985@gmail.com>
2020-12-14 00:41:10 -0300

    punto 8 terminado

--- site2/routes/users.js
+++ site2/routes/users.js
@@ -4,6 +4,8 @@ var userController = require('../controllers/userController');
 const path = require('path');
 const multer = require('multer');
 const validator = require('../middlewares/validator')
+const auth = require('../middlewares/auth');
+const guest = require('../middlewares/guest')
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -17,14 +19,14 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage })
 
 
-router.get('/register', userController.register);
+router.get('/register', guest, userController.register);
 
 router.post('/register',upload.any(), validator.register ,userController.processRegister);
 
-router.get('/login', userController.login);
+router.get('/login', guest, userController.login);
 
 router.post('/login', validator.login, userController.processLogin);
 
-router.get('/logout', userController.logout)
+router.get('/logout', auth ,userController.logout)
 
 module.exports = router;
