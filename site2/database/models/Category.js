657278d239688156a68fba855d47a1fbd38f16f9
Matias <matias.mangano.1985@gmail.com>
2021-01-07 01:29:10 -0300

    modelos de user, category y product hechos

---
+++ site2/database/models/Category.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const alias = "Category";
+    const cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING
+        },
+    }
+
+    const config = {
+        tablename: "categories"
+    }
+
+    const Category = sequelize.define(alias, cols, config);
+
+    Category.associate = function (models) {
+        Category.hasMany(models.Product, {
+            as: 'products',
+            foreignKey: 'category_id',
+        })
+    }
+
+
+}
