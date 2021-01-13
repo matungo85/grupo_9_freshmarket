2f320b851ac4c6723535ef74218b7179037c3228
Matias <matias.mangano.1985@gmail.com>
2021-01-13 00:07:50 -0300

    script de BD y DER agregados

---
+++ site2/sprint 6/structure.sql
@@ -0,0 +1,50 @@
+CREATE SCHEMA fresh_market;
+
+USE fresh_market;
+
+CREATE TABLE users (
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    avatar VARCHAR(255) NOT NULL,
+    phone VARCHAR(30) NOT NULL,
+    DNI VARCHAR(9) NOT NULL,
+    gender VARCHAR(10),
+	rol INT NOT NULL DEFAULT 10,
+    
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP, -- Fecha de alta
+    updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP, -- Fecha de modificación
+    deleted_at DATETIME -- Fecha de borrado del registro completo
+);
+
+CREATE TABLE products (
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+    price DECIMAL UNSIGNED NOT NULL,
+    description TEXT,
+    weight_volume VARCHAR(10),
+    unit VARCHAR(10),
+    discount INT UNSIGNED DEFAULT 0,
+    image VARCHAR(255) NOT NULL,
+    
+    category_id INT UNSIGNED,
+    
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP, -- Fecha de alta
+    updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP, -- Fecha de modificación
+    deleted_at DATETIME -- Fecha de borrado del registro completo
+);
+
+CREATE TABLE categories (
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+    
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP, -- Fecha de alta
+    updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP, -- Fecha de modificación
+    deleted_at DATETIME -- Fecha de borrado del registro completo
+);
+
+
+ALTER TABLE products
+ADD FOREIGN KEY (category_id) REFERENCES categories(id);
