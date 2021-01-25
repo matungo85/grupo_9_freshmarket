35d3be93048c3e04d5d6b5dfd244674d55979d00
Mora-Dav <moradavi@gmail.com>
2021-01-25 18:47:10 -0300

    Capturando los campos del formulario

--- site2/public/js/validator.js
+++ site2/public/js/validator.js
@@ -2,5 +2,17 @@ window.addEventListener ('load', () =>{
 
 console.log('documento vinculado con exito')
 
+let form = document.querySelector('.form-log')
+let name = document.querySelector('#regName')
+let lastname = document.querySelector('#regLastname')
+let tel = document.querySelector('#regPhone')
+let dni = document.querySelector('#regDni')
+let gender = document.querySelector('#regGender')
+let email = document.querySelector('#regEmail')
+let avatar = document.querySelector('#regAvatar')
+let password = document.querySelector('#regPassword')
+
+console.log (email)
+
 });
 
