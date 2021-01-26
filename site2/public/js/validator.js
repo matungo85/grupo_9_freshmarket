7072d7d9acebab732fd3699b7222b17ba1774b25
Mora-Dav <moradavi@gmail.com>
2021-01-26 11:31:42 -0300

    Validacion de campo nombre ok

--- site2/public/js/validator.js
+++ site2/public/js/validator.js
@@ -4,6 +4,7 @@ window.addEventListener ('load', () =>{
 
     let form = document.querySelector('.form-log')
     let name = document.querySelector('#regName')
+    let errorName = document.querySelector('#errorName')
     let lastname = document.querySelector('#regLastname')
     let tel = document.querySelector('#regPhone')
     let dni = document.querySelector('#regDni')
@@ -15,9 +16,21 @@ window.addEventListener ('load', () =>{
     let button = document.querySelector('button')
 
     button.addEventListener('click', function(event){
+        
+        if(name.value.length < 2){
+            errorName.innerHTML = 'El campo debe tener al menos dos caracteres.'            
+        }else{
+           
+            errorName.innerHTML=''
+        }
+        
+        
         event.preventDefault();
-    
-        console.log('Evento capturado con exito')        
+ 
+        
+
+
+         
     });
 
 
