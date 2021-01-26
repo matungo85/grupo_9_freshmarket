1cd5459f58e5e49eea05603616607b6579c266d1
Mora-Dav <moradavi@gmail.com>
2021-01-26 10:40:29 -0300

    envío del formulario capturado

--- site2/public/js/validator.js
+++ site2/public/js/validator.js
@@ -1,18 +1,31 @@
 window.addEventListener ('load', () =>{
 
-console.log('documento vinculado con exito')
+    console.log('documento vinculado con exito')
 
-let form = document.querySelector('.form-log')
-let name = document.querySelector('#regName')
-let lastname = document.querySelector('#regLastname')
-let tel = document.querySelector('#regPhone')
-let dni = document.querySelector('#regDni')
-let gender = document.querySelector('#regGender')
-let email = document.querySelector('#regEmail')
-let avatar = document.querySelector('#regAvatar')
-let password = document.querySelector('#regPassword')
+    let form = document.querySelector('.form-log')
+    let name = document.querySelector('#regName')
+    let lastname = document.querySelector('#regLastname')
+    let tel = document.querySelector('#regPhone')
+    let dni = document.querySelector('#regDni')
+    let gender = document.querySelector('#regGender')
+    let email = document.querySelector('#regEmail')
+    let avatar = document.querySelector('#regAvatar')
+    let password = document.querySelector('#regPassword')
 
-console.log (email)
+    let button = document.querySelector('button')
 
-});
+    button.addEventListener('click', function(event){
+        event.preventDefault();
+    
+        console.log('Evento capturado con exito')        
+    });
+
+
+
+
+
+
+})
+
+
 
