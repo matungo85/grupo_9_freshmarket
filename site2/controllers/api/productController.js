07d0db771d443b33c17890ea5ee529231dc2dc9b
Matias <matias.mangano.1985@gmail.com>
2021-02-11 01:12:50 -0300

    punto 1 de products terminado

--- site2/controllers/api/productController.js
+++ site2/controllers/api/productController.js
@@ -1,15 +1,19 @@
 const db = require('../../database/models')
 var controller = {};
-const sequelize = require('sequelize')
+const sequelize = require('sequelize');
+const { product } = require('../../middlewares/validator');
 
 controller = {
     list: async (req, res) => {
 
-        const products = await db.Product.findAll({include: ["category"]});
-
+        const products = await db.Product.findAll({include: ["category"], attributes: ["id", "name", "description"]});
+        /*, attributes: ["id", "name", "description"]*/
         const categorias = await db.sequelize.query('select distinct categories.name, count(categories.name) as "productos en esta categoria" from products inner join categories on products.category_id = categories.id group by categories.name' )
 
-        console.log(categorias)
+       for (let i = 0; i < products.length; i++) {
+           products[i].setDataValue("url", "http://localhost:3000/api/products/" + products[i].id)
+        }
+               
         let respuesta = {
             meta: {
                 count: products.length,
