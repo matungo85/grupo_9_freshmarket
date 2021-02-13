5733fc13516649ddb567eecdbb75449d00b085eb
Matias <matias.mangano.1985@gmail.com>
2021-02-12 21:20:04 -0300

    api terminada?

--- site2/controllers/api/productController.js
+++ site2/controllers/api/productController.js
@@ -8,6 +8,7 @@ controller = {
 
         const products = await db.Product.findAll({include: ["category"], attributes: ["id", "name", "description"]});
         
+        /*const categorias = await db.Categories.count({attributes: ["name"], group: "name" })*/
         const categorias = await db.sequelize.query('select distinct categories.name, count(categories.name) as "productos en esta categoria" from products inner join categories on products.category_id = categories.id group by categories.name' )
 
        for (let i = 0; i < products.length; i++) {
@@ -17,7 +18,7 @@ controller = {
         let respuesta = {
             meta: {
                 count: products.length,
-                categorias: categorias
+                categorias: categorias[0]
 
             },
             data: products
