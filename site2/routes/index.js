4102fbe61aeb45d627b6cb8aeb0b21c4ef93a82d
Matias <matias.mangano.1985@gmail.com>
2021-02-22 19:21:50 -0300

    productos destacados arreglado

--- site2/routes/index.js
+++ site2/routes/index.js
@@ -4,7 +4,7 @@ const db = require('../database/models')
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const products = await db.Product.findAll();
+  const products = await db.Product.findAll({limit: 3});
   res.render('home', {products: products});
 });
 
