1186b97044974ab0a94a5aa5cbd892276d05ce7a
Matias <matias.mangano.1985@gmail.com>
2021-02-24 23:21:27 -0300

    suma total de productos

--- site2/controllers/api/productController.js
+++ site2/controllers/api/productController.js
@@ -6,7 +6,7 @@ const { product } = require('../../middlewares/validator');
 controller = {
     list: async (req, res) => {
 
-        const products = await db.Product.findAll({include: ["category"], attributes: ["id", "name", "description"]});
+        const products = await db.Product.findAll({include: ["category"], attributes: ["id", "name", "price", "description"]});
         
         /*const categorias = await db.Categories.count({attributes: ["name"], group: "name" })*/
         const categorias = await db.sequelize.query('select distinct categories.name, count(categories.name) as "productos en esta categoria" from products inner join categories on products.category_id = categories.id group by categories.name' )
@@ -14,9 +14,17 @@ controller = {
        for (let i = 0; i < products.length; i++) {
            products[i].setDataValue("url", "http://localhost:3000/api/products/" + products[i].id)
         }
-        const total = products.reduce((acum,product)=> {
-            acum+=product.price
-        })      
+        let total = 0;
+
+        for (let i = 0; i < products.length; i++) {
+            total = total + Number(products[i].price)
+         }
+
+        /*const total = products.reduce((acum,product)=> {
+            return acum+=product.price
+        })   */
+        console.log(products)
+      
         let respuesta = {
             meta: {
                 count: products.length,
